1ea1f887eb57bb290ba657d86aecab23ad7bff67
Shreya Mohanty <shreya.official93@gmail.com>
2024-04-29 13:02:57 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+//const names = require('./names');
+//const sayHi = require('./utils');
+//const data = require('./alternative');
+require('./mind-grenade')
+/*console.log(data);
+sayHi('susan');
+sayHi(names.john);
+sayHi(names.peter);*/
+
---
+++ intro.js
@@ -0,0 +1,8 @@
+const amount = 9;
+if (amount < 10) {
+    console.log('Small number');
+} else  {
+    console.log('Large number');
+}
+
+console.log('hey its my first node app')
